4e862a24396ad6a90d0abf96283b3c88a52348eb
Vladislav Goncharenko <vladislav.goncharenko@phystech.edu>
2022-10-06 10:10:59 +0300

    run pre-commit, prettifyed some code

--- README.md
+++ README.md
@@ -1,39 +1,63 @@
 # Sound_anomaly_detection
+
 ## Problem
+
 ## Datasets
-  ### 1. MIMII
-  
+
+### 1. MIMII
+
 #### Sound dataset for malfunctioning industrial machine investigation and inspection
 
-[1] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori Suefusa, and Yohei Kawaguchi, “MIMII Dataset: Sound Dataset for Malfunctioning Industrial Machine Investigation and Inspection,” arXiv preprint arXiv:1909.09347, 2019.
+[1] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori
+Suefusa, and Yohei Kawaguchi, “MIMII Dataset: Sound Dataset for Malfunctioning
+Industrial Machine Investigation and Inspection,” arXiv preprint
+arXiv:1909.09347, 2019.
 
-[2] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori Suefusa, and Yohei Kawaguchi, “MIMII Dataset: Sound Dataset for Malfunctioning Industrial Machine Investigation and Inspection,” in Proc. 4th Workshop on Detection and Classification of Acoustic Scenes and Events (DCASE), 2019.
-  ### 2. MIMII DUE
-  
+[2] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori
+Suefusa, and Yohei Kawaguchi, “MIMII Dataset: Sound Dataset for Malfunctioning
+Industrial Machine Investigation and Inspection,” in Proc. 4th Workshop on
+Detection and Classification of Acoustic Scenes and Events (DCASE), 2019.
+
+### 2. MIMII DUE
+
 #### Sound dataset for malfunctioning industrial machine investigation and inspection with domain shifts due to changes in operational and environmental conditions
-  
-[1] Yohei Kawaguchi, Keisuke Imoto, Yuma Koizumi, Noboru Harada, Daisuke Niizumi, Kota Dohi, Ryo Tanabe, Harsh Purohit, and Takashi Endo, "Description and Discussion on DCASE 2021 Challenge Task 2: Unsupervised Anomalous Sound Detection for Machine Condition Monitoring under Domain Shifted Conditions," in arXiv e-prints: 2106.04492, 2021. URL
 
-[2] Ryo Tanabe, Harsh Purohit, Kota Dohi, Takashi Endo, Yuki Nikaido, Toshiki Nakamura, and Yohei Kawaguchi, "MIMII DUE: Sound Dataset for Malfunctioning Industrial Machine Investigation and Inspection with Domain Shifts due to Changes in Operational and Environmental Conditions," in arXiv e-prints: 2105.02702, 2021. URL
-  
-  
-  ### 3. ToyADMOS2
-  
-  #### A large-scale dataset for anomaly detection in machine operating sounds (ADMOS), designed for evaluating systems under domain-shift conditions
-  
-[1] Noboru Harada, Daisuke Niizumi, Daiki Takeuchi, Yasunori Ohishi, Masahiro Yasuda, and Shoichiro Saito, "ToyADMOS2: Another dataset of miniature-machine operating sounds for anomalous sound detection under domain shift conditions," 2021. https://arxiv.org/abs/2106.02369
-  
-  ### 4. NAB datasets
-  #### The Numenta Anomaly Benchmark (NAB) datasets for research in streaming anomaly detection
-  NAB is a novel benchmark for evaluating algorithms for anomaly detection in streaming, real-time applications. https://github.com/numenta/NAB
-  
-  ### 5. Bosch CNC Machining Dataset
-  
-  #### A collection of real-world industrial vibration data collected from a brownfield CNC milling machine
-  
-  [1] Smart Data Collection System for Brownfield CNC Milling Machines: A New Benchmark Dataset for Data-Driven Machine Monitoring
-  https://doi.org/10.1016/j.procir.2022.04.022
-  
-  https://github.com/boschresearch/CNC_Machining
-  
+[1] Yohei Kawaguchi, Keisuke Imoto, Yuma Koizumi, Noboru Harada, Daisuke
+Niizumi, Kota Dohi, Ryo Tanabe, Harsh Purohit, and Takashi Endo, "Description
+and Discussion on DCASE 2021 Challenge Task 2: Unsupervised Anomalous Sound
+Detection for Machine Condition Monitoring under Domain Shifted Conditions," in
+arXiv e-prints: 2106.04492, 2021. URL
+
+[2] Ryo Tanabe, Harsh Purohit, Kota Dohi, Takashi Endo, Yuki Nikaido, Toshiki
+Nakamura, and Yohei Kawaguchi, "MIMII DUE: Sound Dataset for Malfunctioning
+Industrial Machine Investigation and Inspection with Domain Shifts due to
+Changes in Operational and Environmental Conditions," in arXiv e-prints:
+2105.02702, 2021. URL
+
+### 3. ToyADMOS2
+
+#### A large-scale dataset for anomaly detection in machine operating sounds (ADMOS), designed for evaluating systems under domain-shift conditions
+
+[1] Noboru Harada, Daisuke Niizumi, Daiki Takeuchi, Yasunori Ohishi, Masahiro
+Yasuda, and Shoichiro Saito, "ToyADMOS2: Another dataset of miniature-machine
+operating sounds for anomalous sound detection under domain shift
+conditions," 2021. https://arxiv.org/abs/2106.02369
+
+### 4. NAB datasets
+
+#### The Numenta Anomaly Benchmark (NAB) datasets for research in streaming anomaly detection
+
+NAB is a novel benchmark for evaluating algorithms for anomaly detection in
+streaming, real-time applications. https://github.com/numenta/NAB
+
+### 5. Bosch CNC Machining Dataset
+
+#### A collection of real-world industrial vibration data collected from a brownfield CNC milling machine
+
+[1] Smart Data Collection System for Brownfield CNC Milling Machines: A New
+Benchmark Dataset for Data-Driven Machine Monitoring
+https://doi.org/10.1016/j.procir.2022.04.022
+
+https://github.com/boschresearch/CNC_Machining
+
 ## Key Algorithms
--- ds_project/preprocess.py
+++ ds_project/preprocess.py
@@ -1,33 +1,21 @@
-import librosa
 import librosa.core
-import librosa.feature
 import librosa.display
+import librosa.feature
 import numpy as np
 import pandas as pd
-import scipy
-
-import yaml
-import logging
-import os
-import glob
-
-from sklearn import metrics
+from pyod.models.gmm import GMM
 from pyod.models.iforest import IForest
 from pyod.models.knn import KNN
 from pyod.models.lof import LOF
-from pyod.models.gmm import GMM
 from pyod.models.ocsvm import OCSVM
-from pyod.utils.data import generate_data
-from pyod.utils.data import evaluate_print
-from pyod.utils.example import visualize
-
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from tqdm import tqdm
+from pyod.utils.data import evaluate_print, generate_data
+from sklearn import metrics
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 # stratified dataset split
-def StratifiedKF(X, y):
+def stratifiedkf(X, y):
     """
     X - data
     y - labels
@@ -37,7 +25,12 @@ def StratifiedKF(X, y):
     for idx in skf.split(X, y):
         idx_list.append(idx)
         train_idx, test_idx = idx_list[0]
-    X_train, X_test, y_train, y_test = X[train_idx], X[test_idx], y[train_idx], y[test_idx]
+    X_train, X_test, y_train, y_test = (
+        X[train_idx],
+        X[test_idx],
+        y[train_idx],
+        y[test_idx],
+    )
 
     return X_train, X_test, y_train, y_test
 
@@ -70,6 +63,7 @@ def scaling(X_train, X_test, X_val=None):
 
     return X_train, X_test, X_val
 
+
 def anomaly_detection(model_name, X_train, X_test, y_train, y_test):
 
     clf = models[model_name]
@@ -89,19 +83,38 @@ def anomaly_detection(model_name, X_train, X_test, y_train, y_test):
     recall = metrics.recall_score(y_test, y_test_pred)
     f1_score = metrics.f1_score(y_test, y_test_pred)
     scores = pd.DataFrame(
-        [{'Dataset': None, 'Extraction_type': None, 'Model_name': model_name, 'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1_score': f1_score}])
+        [
+            {
+                "Dataset": None,
+                "Extraction_type": None,
+                "Model_name": model_name,
+                "Accuracy": accuracy,
+                "Precision": precision,
+                "Recall": recall,
+                "F1_score": f1_score,
+            }
+        ]
+    )
     # evaluate and print the results
     report = metrics.classification_report(y_test, y_test_pred)
-    #print(f'On Test Data:')
+    # print(f'On Test Data:')
     evaluate_print(model_name, y_test, y_test_scores)
-    #print(f'\n{report} \n')
+    # print(f'\n{report} \n')
 
     return scores
 
 
-# Key algorithms, PyOD implementation
-def PyOD_classification_report(X_train, X_test, y_train, y_test, dataset=None, extraction_type='aggregate_MFCC', contamination=0.1):
-    """
+def pyod_classification_report(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    dataset=None,
+    extraction_type="aggregate_MFCC",
+    contamination=0.1,
+):
+    """Key algorithms, PyOD implementation
+
     dataset : str
         'MIMII_DUE'
         'ToyAdm'
@@ -110,24 +123,36 @@ def PyOD_classification_report(X_train, X_test, y_train, y_test, dataset=None, e
         'amplitude' - original signal, amplitude values timeseries
 
     """
-    models = {'IForest': IForest(behaviour='old',
-                                 contamination=contamination,
-                                 max_features=max(1,int(X_train.shape[1]//2)),
-                                 max_samples='auto',
-                                 n_estimators=10,
-                                 n_jobs=-1,
-                                 random_state=42,
-                                 verbose=0),
-              'LOF': LOF(contamination=contamination),
-              'KNN': KNN(contamination=contamination),
-              'GMM': GMM(contamination=contamination),
-              'OCSVM': OCSVM(contamination=0.1)
-              }
+    models = {
+        "IForest": IForest(
+            behaviour="old",
+            contamination=contamination,
+            max_features=max(1, int(X_train.shape[1] // 2)),
+            max_samples="auto",
+            n_estimators=10,
+            n_jobs=-1,
+            random_state=42,
+            verbose=0,
+        ),
+        "LOF": LOF(contamination=contamination),
+        "KNN": KNN(contamination=contamination),
+        "GMM": GMM(contamination=contamination),
+        "OCSVM": OCSVM(contamination=0.1),
+    }
 
     pyod_models = pd.DataFrame(
-        columns={'Dataset', 'Extraction_type', 'Model_name', 'Accuracy', 'Precision', 'Recall', 'F1_score'})
-    for model_name, model in models.items():
-        if extraction_type == 'amplitude' and model_name in ['KNN', 'GMM']:
+        columns={
+            "Dataset",
+            "Extraction_type",
+            "Model_name",
+            "Accuracy",
+            "Precision",
+            "Recall",
+            "F1_score",
+        }
+    )
+    for model_name in models.keys():
+        if extraction_type == "amplitude" and model_name in ["KNN", "GMM"]:
             continue
 
         clf = models[model_name]
@@ -143,9 +168,20 @@ def PyOD_classification_report(X_train, X_test, y_train, y_test, dataset=None, e
         recall = metrics.recall_score(y_test, y_test_pred)
         f1_score = metrics.f1_score(y_test, y_test_pred)
         scores = pd.DataFrame(
-            [{'Dataset': None, 'Extraction_type': None, 'Model_name': model_name, 'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1_score': f1_score}])
+            [
+                {
+                    "Dataset": None,
+                    "Extraction_type": None,
+                    "Model_name": model_name,
+                    "Accuracy": accuracy,
+                    "Precision": precision,
+                    "Recall": recall,
+                    "F1_score": f1_score,
+                }
+            ]
+        )
         pyod_models = pyod_models.append(scores)
-        pyod_models['Dataset'] = dataset
-        pyod_models['Extraction_type'] = extraction_type
+        pyod_models["Dataset"] = dataset
+        pyod_models["Extraction_type"] = extraction_type
 
     return pyod_models
